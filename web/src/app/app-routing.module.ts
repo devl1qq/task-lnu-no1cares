a915049c0298f067e701a8624f8d27ee1b987857
mterekhovskyi <mterekhosvkyi@artelogic.net>
2023-10-16 10:39:47 +0300

    - update auth layout - add draft for routing - fix grid layout in order to look a liitle bit similar to figma

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { ItemsListComponent } from './items-list/items-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/list',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
+  },
+  {
+    path: 'list',
+    component: ItemsListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
