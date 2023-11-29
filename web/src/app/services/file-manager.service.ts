1d5b5f535a7997b3201878e1948c571bcace9f8f
Misha <62945368+MishaFi@users.noreply.github.com>
2023-11-30 00:12:28 +0200

    UPD file-manager.service.ts
    Додав константи, інтерфейси та обробку помилок. Також зробив код більш читабельним та підтримуваним.

--- web/src/app/services/file-manager.service.ts
+++ web/src/app/services/file-manager.service.ts
@@ -1,22 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Item } from '../utils/file';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import * as moment from 'moment';
+
+export interface Item {
+  name: string;
+  date: string;
+  type: string;
+  size: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class FileManagerService {
-  constructor(private _http: HttpClient) {}
+  private readonly shoesPath = 'Home/Products/Shoes';
+  private readonly runningShoesPath = 'Home/Products/Shoes/Running Shoes';
 
-  getFoldersContent(path: string): Item[] {
-    // : Observable<Item[]>
-    // mock api call
-    // return this._http.get('https://jsonplaceholder.typicode.com/todos/1')
+  private generateMockData(type: string, count: number): Item[] {
+    return Array.from({ length: count }, (_, index) => ({
+      type,
+      name: `${type} ${index + 1}`,
+      date: moment.utc(new Date()).format('yyyy-MM-dd'),
+      size: '1.5 MB',
+    }));
+  }
 
-    if (path === 'Home/Products/Shoes') {
-      return [
+  getFoldersContent(path: string): Observable<Item[]> {
+    if (path === this.shoesPath) {
+      return of([
         {
           name: 'Running Shoes',
           date: moment.utc(new Date()).format('yyyy-MM-dd'),
@@ -35,42 +48,14 @@ export class FileManagerService {
           type: 'folder',
           size: '1.5 MB',
         },
-      ];
-    } else if (path === 'Home/Products/Shoes/Running Shoes') {
-      return [
-        {
-          type: 'folder',
-          name: 'Folder 1',
-          date: '2021-01-01',
-          size: '1.5 MB',
-        },
-        {
-          type: 'folder',
-          name: 'Folder 1',
-          date: '2021-01-01',
-          size: '1.5 MB',
-        },
-        {
-          type: 'folder',
-          name: 'Folder 1',
-          date: '2021-01-01',
-          size: '1.5 MB',
-        },
-        {
-          type: 'folder',
-          name: 'Folder 1',
-          date: '2021-01-01',
-          size: '1.5 MB',
-        },
-        {
-          type: 'folder',
-          name: 'Folder 1',
-          date: '2021-01-01',
-          size: '1.5 MB',
-        },
-      ];
+      ]);
+    } else if (path === this.runningShoesPath) {
+      return of(this.generateMockData('folder', 5));
+    } else if (path.startsWith(`${this.runningShoesPath}/Folder`)) {
+      const folderNumber = +path.split('/').pop();
+      return of(this.generateMockData('file', folderNumber * 3));
     } else {
-      return [];
+      return throwError('Invalid path');
     }
   }
 }
