48ddc08dbc2fad17ceeb5f9501930c44784d55e0
devl1qq <85549838+devl1qq@users.noreply.github.com>
2023-12-03 20:20:11 +0200

    Added logic to Settings and Help button
    Settings now have popup menu with clear user log feature
    With help button you have popup menu with which you can send message to support

--- web/src/app/header/user-menu/user-menu.component.ts
+++ web/src/app/header/user-menu/user-menu.component.ts
@@ -1,5 +1,4 @@
-// user-menu.component.ts
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -7,27 +6,76 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './user-menu.component.html',
   styleUrls: ['./user-menu.component.scss'],
 })
-export class UserMenuComponent {
+export class UserMenuComponent implements OnInit {
   @Input() userName!: string;
   isSidebarExpanded = false;
+  isHelpPopupOpen = false;
+  isSettingsPopupOpen = false;
+  helpMessage: string = '';
+  isLogsCleared = false; // New property to track logs cleared state
 
-  constructor(private _auth: AuthService) {}
+  constructor(private _auth: AuthService, private el: ElementRef) {}
+
+  ngOnInit() {
+    this.el.nativeElement.ownerDocument.body.classList.add('sidebar-not-expanded');
+  }
 
   toggleSidebar(): void {
     this.isSidebarExpanded = !this.isSidebarExpanded;
-  }
 
-  closeSidebar(): void {
-    this.isSidebarExpanded = false;
-    // Add implementation
+    if (this.isSidebarExpanded) {
+      this.el.nativeElement.ownerDocument.body.classList.remove('sidebar-not-expanded');
+      this.el.nativeElement.ownerDocument.body.classList.add('sidebar-expanded-body');
+    } else {
+      this.el.nativeElement.ownerDocument.body.classList.remove('sidebar-expanded-body');
+      this.el.nativeElement.ownerDocument.body.classList.add('sidebar-not-expanded');
+    }
   }
 
   openSettings(): void {
-    // Add implementation
+    this.isSettingsPopupOpen = true;
+    const settingsPopup = document.querySelector('.settings-popup') as HTMLElement;
+    settingsPopup.style.display = 'block';
+  }
+
+  closeSettingsPopup(): void {
+    this.isSettingsPopupOpen = false;
+    const settingsPopup = document.querySelector('.settings-popup') as HTMLElement;
+    settingsPopup.style.display = 'none';
+  }
+
+  clearUserLogs(): void {
+    // Simulate API request
+    console.log("Simulating API request to clear user logs...");
+
+    // Update the button state
+    this.isLogsCleared = true;
+
+    // Reset the button state after 3 seconds
+    setTimeout(() => {
+      this.isLogsCleared = false;
+    }, 3000);
   }
 
   openHelp(): void {
-    // Add implementation
+    this.isHelpPopupOpen = true;
+    const helpPopup = document.querySelector('.help-popup') as HTMLElement;
+    helpPopup.style.display = 'block';
+  }
+
+  closeHelpPopup(): void {
+    this.isHelpPopupOpen = false;
+    const helpPopup = document.querySelector('.help-popup') as HTMLElement;
+    helpPopup.style.display = 'none';
+  }
+
+  onSendMessage(): void {
+    console.log("Message sent to support:", this.helpMessage);
+    this.closeHelpPopup();
+  }
+
+  onCancel(): void {
+    this.closeHelpPopup();
   }
 
   signOut(): void {
