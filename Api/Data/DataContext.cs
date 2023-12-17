af750c1406211e600c364f77dfc610dead5a4be0
Misha <62945368+MishaFi@users.noreply.github.com>
2023-12-17 23:31:12 +0200

    Оновив відповідно до User.cs

--- Api/Data/DataContext.cs
+++ Api/Data/DataContext.cs
@@ -1,14 +1,21 @@
-﻿using Api.Data.Entities;
+using Api.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api.Data;
 
-public class DataContext: DbContext
+public class DataContext : DbContext
 {
     public DbSet<Item> Items { get; set; }
+    public DbSet<User> Users { get; set; }
 
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
 
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Item>().ToTable("Items");
+        modelBuilder.Entity<User>().ToTable("Users");
     }
 }
